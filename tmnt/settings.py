44813f40555c751a63aac7bb64d90818f5008edb
Cahlan Sharp <cahlan@gmail.com>
2014-02-05 12:20:01 -0700

    didn't work

--- tmnt/settings.py
+++ tmnt/settings.py
@@ -129,10 +129,6 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 )
 
-REST_FRAMEWORK = {
-  'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',))
-}
-
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
